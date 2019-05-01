3a8e865b15da2707e17e91e0d9909c910fb63775
Gordei Evgenii <gordei.evgenii@iqubecompany.com>
2019-05-01 17:22:11 +0300

    animations

--- BlockchainLand/src/assets/js/init.js
+++ BlockchainLand/src/assets/js/init.js
@@ -46,23 +46,83 @@
 	// 	return false;
 	// });
 
-	let appArea = new TimelineMax({paused: true});
+	function animations() {
+		let appArea = new TimelineMax({paused: true});
+		let anim_appArea = appArea
+			.addLabel("start", "+=0")
+			.staggerFrom(".app-area__slider-marker-item", 0.8, {
+				y: 80,
+				ease: Expo.easeOut,
+				delay: 0,
+				opacity: 0
+			}, 0.15)
+			.from('.icon-energo .body', 0.3, {
+				y: 100,
+				opacity: 0
+			}, "start+=0.7")
+			.from('.icon-energo .inner', 0.3, {
+				opacity: 0
+			}, "start+=1")
+			.from('.icon-energo .frame1', 0.3, {
+				y: 20,
+				opacity: 0
+			}, "start+=1.3")
+			.from('.icon-energo .frame2', 0.3, {
+				y: 20,
+				opacity: 0
+			}, "start+=1.6")
 
-	var anim_appArea = appArea
-		.from('.app-area', 1, {
-			y: 50
-		}, '-=0.2')
-		.staggerFrom(".app-area__slider-marker-item", 0.8, {
-			y: -100,
-			ease: Expo.easeOut,
-			delay: 0,
-			opacity: 0
-		}, 0.15);
+		$(".app-area").waypoint(function () {
+			anim_appArea.resume();
+		}, {
+			offset: '82%'
+		});
+	
+		let aiml = new TimelineMax({paused: true});
+		let anim_aiml = aiml
+			.staggerFrom(".aiml__slider-item", 0.6, {
+				opacity: 0
+			}, 0.2);
+		$(".aiml").waypoint(function () {
+			anim_aiml.resume();
+		}, {
+			offset: '82%'
+		});
+	
+		let blockchain = new TimelineMax({paused: true});
+		let anim_blockchain = blockchain
+			.from(".blockchain", 1.2, {
+				x: 300,
+				ease: Expo.easeOut,
+				delay: 0,
+				opacity: 0
+			}, 0);
+	
+		$(".blockchain").waypoint(function () {
+			anim_blockchain.resume();
+		}, {
+			offset: '85%'
+		});
+	
+		let map = new TimelineMax({paused: true});
+		let anim_map = map
+			.from(".map", 1.2, {
+				x: -300,
+				ease: Expo.easeOut,
+				delay: 0,
+				opacity: 0
+			}, 0);
+	
+		$(".map").waypoint(function () {
+			anim_map.resume();
+		}, {
+			offset: '85%'
+		});
+	}
 
-	$(".app-area").waypoint(function () {
-		anim_appArea.resume();
-	}, {
-		offset: '65%'
-	});
+	if ($(window).width() > 1023) {
+		animations();
+	}
+
 
 })();
